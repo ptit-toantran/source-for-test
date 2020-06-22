7a045f8336fd6735c9995151bd0c993bc2bae65c
Toan Tran <tranvantoan.ptit@gmail.com>
2020-06-22 17:28:34 +0700

    commit

---
+++ type-test/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { TodoListItem } from './TodoListItem';
+const todos: Todo[] = [
+  {
+    text: 'Walk the dog',
+    complete: false,
+  },
+  {
+    text: 'Write app',
+    complete: true,
+  },
+];
+function App() {
+  return(
+  <ul>
+    <TodoListItem todo={todos[0]} />
+    <TodoListItem todo={todos[1]} />
+  </ul>
+  );
+}
+export default App;
